707211326500e23c436008ef2650f2dce26bc365
Chris Bigg <christopherbigg@gmail.com>
2021-12-08 18:21:16 +0000

    remove hero

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Hero from "./Hero.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hero />
     <App />
   </React.StrictMode>,
   document.getElementById("root")

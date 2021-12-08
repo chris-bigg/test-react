4812e927cf73198be19c1fbbc16008076f4a975e
Chris Bigg <christopherbigg@gmail.com>
2021-12-08 21:49:43 +0000

    changed app.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo object-none" alt="logo" />
-        <p className="bg-green-500 p-8 rounded-full">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="my-5 bg-red-500 border-2 hover:bg-transparent hover:border-green-500 py-2 px-4 rounded-full transition-all"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
